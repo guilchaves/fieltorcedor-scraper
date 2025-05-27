705a463ec1f8422716c4d0658f34c418e7b4570c
guilherme chaves <chaves.guilherme@proton.me>
2025-05-27 16:53:40 -0300

    feat: add recurring job

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -4,23 +4,27 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/guilchaves/fieltorcedorbot/internal/adapters/in"
 	"github.com/guilchaves/fieltorcedorbot/internal/adapters/out/fieltorcedor"
 	"github.com/guilchaves/fieltorcedorbot/internal/adapters/out/notification"
 	"github.com/guilchaves/fieltorcedorbot/internal/core/service"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Println("Erro ao carregar o arquivo .env. Variáveis de ambiente podem não estar definidas.")
+		log.Println(
+			"Erro ao carregar o arquivo .env. Variáveis de ambiente podem não estar definidas.",
+		)
 	}
 
 	telegramToken := os.Getenv("TELEGRAM_BOT_TOKEN")
 	telegramChatID := os.Getenv("TELEGRAM_CHAT_ID")
 	if telegramToken == "" || telegramChatID == "" {
-		log.Fatal("TELEGRAM_BOT_TOKEN e TELEGRAM_CHAT_ID devem estar definidos nas variáveis de ambiente")
+		log.Fatal(
+			"TELEGRAM_BOT_TOKEN e TELEGRAM_CHAT_ID devem estar definidos nas variáveis de ambiente",
+		)
 	}
 
 	siteURL := "https://www.fieltorcedor.com.br/"
@@ -35,6 +39,17 @@ func main() {
 	if err != nil {
 		log.Fatalf("Erro ao iniciar o scheduler: %v", err)
 	}
+
+	dailyScheduler := in.NewScheduler(gameService, "0 11 * * *")
+	if err := dailyScheduler.Start(); err != nil {
+		log.Fatalf("erro ao iniciar o scheduler diário: %v", err)
+	}
+
+	go func() {
+		if _, err := gameService.CheckForNewGames(); err != nil {
+			log.Printf("erro na verificação inicial: %v", err)
+		}
+	}()
 
 	select {}
 }
